2c0d6de99f7b6eb10440921e2c3a5b5a5ac4e55b
PERUZZI GIOVANNI <12999427@itisrossi.edu.it>
2025-01-29 16:01:43 +0100

    modifichette

---
+++ app/products/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1>
+        Ciao
+      </h1>
+      <button className="">
+        Tailwind Button
+      </button>
+    </>
+  );
+}
